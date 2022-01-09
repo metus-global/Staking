7ef2c71e8a05515186c46d7819b92484b80745ff
MetaDAO <68357603+metadao-space@users.noreply.github.com>
2022-01-09 10:54:50 +0100

    update frontend
    testing-frontend

---
+++ build/precache-manifest.65ab50acfbce85b2b977e54f1e6fd20b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61f717f3035b1184cf14310d51e3732e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7eeebaa660e3f44bc80d",
+    "url": "/static/css/main.04d14f80.chunk.css"
+  },
+  {
+    "revision": "8171b5310f921b8aa066",
+    "url": "/static/js/2.3d7f2387.chunk.js"
+  },
+  {
+    "revision": "861d07e5208809e8c3f2bd07bc3c6e3e",
+    "url": "/static/js/2.3d7f2387.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7eeebaa660e3f44bc80d",
+    "url": "/static/js/main.676d7cb1.chunk.js"
+  },
+  {
+    "revision": "f63fe6a39890fdad3b18",
+    "url": "/static/js/runtime-main.1121f74d.js"
+  }
+]);
